11e6d4c91f252d030b8e4ea92202462677c5bae4
propertyofkeow <marta.sibielak92@gmail.com>
2019-07-16 13:01:55 +0200

    fix type mistake

--- src/main/java/UserDaoImpl.java
+++ src/main/java/UserDaoImpl.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.List;
 
-public class UserDaoImpl {
+public class UserDaoImpl implements UserDao{
+
+    private List<User> users;
+
+
+    @Override
+    public List getAllUser() {
+        return null;
+    }
+
+    @Override
+    public void deleteUser() {
+
+    }
+
+    @Override
+    public void addUser() {
+
+    }
 }

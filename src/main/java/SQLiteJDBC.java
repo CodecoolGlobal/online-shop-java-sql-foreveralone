c493d3f25c112d78ca1978a30686651acc9bf3da
propertyofkeow <marta.sibielak92@gmail.com>
2019-07-16 14:14:27 +0200

    connect database to java

--- src/main/java/SQLiteJDBC.java
+++ src/main/java/SQLiteJDBC.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import  java.sql.*;
 
 public class SQLiteJDBC {
+
+
+    Connection c = null;
+
+
+    public void connect(){
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:onlineshop.db");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("opened database succesfully");
+    }
+
+    public void dissconnect(){
+        try {
+            c.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }

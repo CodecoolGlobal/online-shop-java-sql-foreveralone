9dfc986d12e8f95eac923cb789056a14f68e6977
propertyofkeow <marta.sibielak92@gmail.com>
2019-07-17 11:53:22 +0200

    create SqlImplementation class

--- src/main/java/SqlImplementation.java
+++ src/main/java/SqlImplementation.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class SqlImplementation {
+public class SqlImplementation implements SQL{
+
+    SQLiteJDBC connect;
+    Connection con;
+    Statement statement;
+
+    public SqlImplementation(){
+        connect = new SQLiteJDBC();
+    }
+
+
+
+    public ResultSet selectQuery(String sql) {
+        ResultSet resultSet = null;
+        try {
+            con = connect.connect();
+            statement = con.createStatement();
+            resultSet = statement.executeQuery(sql);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+
+    public void doQuery(String sql) {
+        try {
+            con = connect.connect();
+            statement = con.createStatement();
+            statement.executeUpdate(sql);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public void closeQuery() {
+        try {
+            this.statement.close();
+            this.con.close();
+            this.connect.disconnect();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }

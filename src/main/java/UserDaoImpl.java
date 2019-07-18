bb8f0adcc62f48b83729eb4c8bb7ae96ac9f821e
propertyofkeow <marta.sibielak92@gmail.com>
2019-07-18 09:40:02 +0200

    modify addUser function

--- src/main/java/UserDaoImpl.java
+++ src/main/java/UserDaoImpl.java
@@ -1,22 +1,63 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.sql.*;
 
-public class UserDaoImpl implements UserDao{
+public class UserDaoImpl implements UserDao {
 
-    private List<User> users;
+    SqlImplementation sqlImplementation;
+
+    public UserDaoImpl(){
+        sqlImplementation = new SqlImplementation();
+    }
 
 
     @Override
-    public List getAllUser() {
-        return null;
+    public List getData(){
+
+        ResultSet resultSet = sqlImplementation.selectQuery("select * from user");
+        List<User> userList = new ArrayList<>();
+        try {
+            while (resultSet.next()){
+                int userId = resultSet.getInt("user_id");
+                String userName = resultSet.getString("user_name");
+                String userPassword = resultSet.getString("user_password");
+                int isAdmin = resultSet.getInt("is_admin");
+
+                User user = new User(userName, userPassword, userId, isAdmin);
+                userList.add(user);
+            }
+
+        } catch(Exception e) {
+            System.out.println(e);
+        } finally {
+            sqlImplementation.closeQuery();
+        }
+        return userList;
     }
 
     @Override
-    public void deleteUser() {
+    public List<User> getAllUser() {
+        List<User> userList = new ArrayList<>();
+        return userList;
+    }
+
+
+    @Override
+    public void deleteUser(User user) {
+
 
     }
 
     @Override
-    public void addUser() {
+    public void addUser(User user) {
+
+        try {
+            sqlImplementation.doQuery("insert into user (user_name, user_password, user_id) VALUES ('"+ user.getName() + "','"+ user.getPassword() +"', '"+ user.getUserId() +"')");
+        } catch (Exception e){
+            System.out.println(e);
+        } finally {
+            sqlImplementation.closeQuery();
+        }
 
     }
 

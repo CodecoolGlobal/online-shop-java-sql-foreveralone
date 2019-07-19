0310892e87d050049137e5806481d24271012452
propertyofkeow <marta.sibielak92@gmail.com>
2019-07-19 11:12:15 +0200

    create constructor for Category class

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -1,2 +1,22 @@
+import java.util.List;
+
 public class Category {
+
+    private Integer id;
+    private String name;
+    private Boolean isAvailable;
+    private List<Product> products;
+
+
+    public Category (String name){
+
+    }
+
+    public GetProducts(List<Product> products){
+        return;
+    }
+
+    public String ToString(){
+        return;
+    }
 }
